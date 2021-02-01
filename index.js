14f8a62a0bb7d40c0cba6d146da9f3fa5748aaed
Coder246 <tim-arnold@outlook.de>
2021-02-01 13:17:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 var mysql      = require('mysql');
+var express        = require('express');
+const app = express()
+const port = 8146
+
+
 var connection = mysql.createConnection({
   host     : '192.168.2.146',
   user     : 'phpmyadmin',
-  password : '****s'
+  password : '*****++'
 });
 
 connection.connect();
+
+
+
+app.get('/', (req, res) => {
+  res.send('LEDWall API V1.0')
+})
+
+app.get('/auth', (req,res) => {
+
+  res.send('please provide credentials')
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
 
 connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
   if (err) throw err;
@@ -13,3 +36,4 @@ connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
 });
 
 connection.end();
+
