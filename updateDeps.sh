f82c524c02b50d0ca564c4ad9ad9df0f200d0bce
Coder246 <46609883+Coder246@users.noreply.github.com>
2021-08-28 22:14:19 +0200

    Added Update Script

---
+++ updateDeps.sh
@@ -0,0 +1,23 @@
+cd account
+npm update
+cd ..
+
+cd device
+npm update
+cd ..
+
+cd drone
+npm update
+cd ..
+
+cd ledwall
+npm update
+cd ..
+
+cd MainService
+npm update
+cd ..
+
+cd search
+npm update
+cd ..
